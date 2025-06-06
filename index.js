1b4d5d03311db5ec2fe9d4bf6c5647828774dba0
Manish20060829 <Shotgunfanboy@gmail.com>
2025-06-06 19:40:42 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+dayjs.extend(dayjs_plugin_utc);
+
+dayjs.extend(dayjs_plugin_timezone);
+
+
+const timelist = Intl.supportedValuesOf("timeZone");
+
+console.log(timelist);
+
+console.log(dayjs().tz("Africa/Nouakchott").format());
+
+timelist.forEach(myfunction);
+
+function myfunction(item)
+{
+const index = item.indexOf("/");
+
+const continenet = item.slice(0,index);
+const country = item.slice(index+1);
+
+console.log(continenet);
+console.log(country);
+
+const group = document.getElementById(continenet);
+const option  = document.createElement("option");
+
+option.value = item;
+option.textContent = country.replaceAll("/", ": ").replaceAll("_", " ");
+
+group.append(option);
+
+
+
+
+}
+
+setInterval(displaytime, 1000);
+
+function displaytime(){
+    let select = document.getElementById("select").value;
+   
+
+
+   date = dayjs().tz(select).format('dddd, D MMMM YYYY');
+   time = dayjs().tz(select).format('h:mm:ssA');
+
+   const date_html = document.getElementById("date");
+   const time_html = document.getElementById("time");
+
+   date_html.textContent = date;
+   time_html.textContent = time;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
