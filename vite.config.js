58807d8c47e6c123583ec144ec40ebcab3093b08
Manish20060829 <Shotgunfanboy@gmail.com>
2025-06-07 18:12:11 -0400

    repo updated

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/timezoneapp/', // <-- use your repo name here
+  base: '/Timezoneapp/', // <-- use your repo name here
 });
